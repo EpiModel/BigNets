ea6c7dde7a8a21df97f5354c1d1ac2a7ac887e8d
Samuel Jenness <smjenness@gmail.com>
2021-12-13 13:30:37 -0500

    Update run scripts

--- R/03-burnin1-simMin.R
+++ R/03-burnin1-simMin.R
@@ -13,9 +13,9 @@ suppressMessages(library("EpiModelHPC"))
 pull_env_vars()
 
 ## Parameters
-netstats <- readRDS("data/input/netstats-100k.rds")
+netstats <- readRDS("data/input/netstats-10k.rds")
 epistats <- readRDS("data/input/epistats.rds")
-est <- readRDS("data/input/netest-100k.rds")
+est <- readRDS("data/input/netest-10k.rds")
 
 param <- param_msm(netstats = netstats,
                    epistats = epistats,
@@ -30,8 +30,8 @@ param <- param_msm(netstats = netstats,
 init <- init_msm()
 control <- control_msm(simno = fsimno,
                        nsteps = 52 * 60,
-                       nsims = 1,
-                       ncores = 1)
+                       nsims = ncores,
+                       ncores = ncores)
 ## Simulation
 sim <- netsim(est, param, init, control)
 
@@ -39,4 +39,4 @@ sim <- netsim(est, param, init, control)
 savesim(sim, save.min = TRUE, save.max = FALSE, compress = TRUE)
 
 # Merging
-# process_simfiles(simno = simno, min.n = njobs, nsims = nsims)
+process_simfiles(simno = simno, min.n = njobs, nsims = nsims)
--- main.sh
+++ main.sh
@@ -7,3 +7,6 @@ sbatch -N 1 -n 1 --cpus-per-task=32 -p preemptable --time=24:00:00 --mem=180G --
 # sbatch -N 1 -n 1 --cpus-per-task=32 -p preemptable --time=24:00:00 --mem=180G --job-name=dx --dependency=afterok:3677314 rundx.sh
 
 sbatch -p preemptable --array=1-20 --nodes=1 --cpus-per-task=32 --time=24:00:00 --mem=185G --job-name=s1 --mail-type=ALL --export=ALL,SIMNO=0001 sh/runR.sh "03-burnin1-simMin"
+
+sbatch -p preemptable --array=1-2 --nodes=1 --ntasks-per-node=32 --time=02:00:00 --mem=180G --job-name=s2
+                      --export=ALL,SIMNO=0002,NJOBS=2,NSIMS=60 sh/runsim.sh "03-burnin1-simMin"
