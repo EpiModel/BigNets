4e7dd42f58258790b123eed25a9a3ac10f74d8ec
Samuel Jenness <smjenness@gmail.com>
2021-12-15 11:21:57 -0500

    Remove R scripts 04-07

--- sh/scp.sh
+++ sh/scp.sh
@@ -6,3 +6,4 @@ scp R/*.R sph:/projects/epimodel/sjenness/BigNets/R
 # Receive
 
 scp sph:/projects/epimodel/sjenness/BigNets/data/input/*.rds data/input/
+scp sph:/projects/epimodel/sjenness/BigNets/data/output/*.rda data/output/
