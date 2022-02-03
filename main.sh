216ccf346b7f3159e7491680a90918831aa7ad48
Samuel Jenness <smjenness@gmail.com>
2022-02-02 20:57:28 -0500

    Update shell scripts

--- main.sh
+++ main.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-sbatch -N 1 -n 1 --cpus-per-task=32 -p preemptable --time=24:00:00 --mem=180G --job-name=est sh/runR.sh "01-estimation"
+sbatch -p preemptable -c 32 -t 24:00:00 --mem=0 -J est sh/runR.sh "01-estimation"
 
-sbatch -N 1 -n 1 --cpus-per-task=32 -p preemptable --time=24:00:00 --mem=180G --job-name=dx --mail-type=ALL sh/runR.sh "02-diagnostics"
+sbatch -p preemptable -c 32 -t 24:00:00 --mem=0 -J dx sh/runR.sh "02-diagnostics"
 
-# sbatch -N 1 -n 1 --cpus-per-task=32 -p preemptable --time=24:00:00 --mem=180G --job-name=dx --dependency=afterok:3677314 rundx.sh
+sbatch -p preemptable -c 32 -t 24:00:00 --mem=0 -J -d afterok:3677314 rundx.sh
 
-sbatch -p preemptable --array=1-20 --nodes=1 --cpus-per-task=32 --time=24:00:00 --mem=185G --job-name=s1 --mail-type=ALL --export=ALL,SIMNO=0001 sh/runR.sh "03-burnin1-simMin"
+sbatch -p preemptable -a 1-20 -c 32 -t 24:00:00 --mem=0 -J s1 --export=ALL,SIMNO=0001 sh/runR.sh "03-burnin1-simMin"
 
-sbatch -p preemptable --array=1-20 --nodes=1 --cpus-per-task=32 --time=24:00:00 --mem=180G --job-name=s3100 \
-       --mail-type=ALL --mail-user=sjennes@emory.edu --export=ALL,SIMNO=3100,NETSIZE=100 \
-       sh/runR.sh "03-burnin1-simMin"
+sbatch -p preemptable -a 1-20 -c 32 -t 24:00:00 --mem=0 -J s3100 --export=ALL,SIMNO=3100,NETSIZE=100 sh/runR.sh "03-burnin1-simMin"
