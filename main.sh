ed2136513b76bc01f90ab62526c3c1d0a28ce948
Samuel Jenness <smjenness@gmail.com>
2022-03-04 15:58:55 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-sbatch -p preemptable -c 32 -t 24:00:00 --mem=0 -J est sh/runR.sh "01-estimation"
+sbatch -p epimodel -c 32 -t 24:00:00 --mem=0 -J est --export=ALL,NETSIZE=102000 sh/runR.sh "01-estimation"
 
-sbatch -p preemptable -c 32 -t 24:00:00 --mem=0 -J dx sh/runR.sh "02-diagnostics"
-
-sbatch -p preemptable -c 32 -t 24:00:00 --mem=0 -J -d afterok:3677314 rundx.sh
+sbatch -p epimodel -c 32 -t 24:00:00 --mem=0 -J dx -d afterok:4695323 --export=ALL,NETSIZE=102000 sh/runR.sh "02-diagnostics"
 
 sbatch -p preemptable -a 1-20 -c 32 -t 24:00:00 --mem=0 -J s1 --export=ALL,SIMNO=0001 sh/runR.sh "03-burnin1-simMin"
 
